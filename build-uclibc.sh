3377cfd90fed0460cc21ed44b73ed44cf472a273
robacklin <rob.acklin@gmail.com>
2013-01-14 22:45:06 -0500

    added if statement back to build-uclibc.sh

--- build-uclibc.sh
+++ build-uclibc.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 
+if [ -d "moonshine" ] ; then
+	cd moonshine ; git pull
+	cd ..
+else
+	git clone git://github.com/robacklin/moonshine.git
+fi
+
 sh ./moonshine/scripts/build-uclibc.sh
 
 
